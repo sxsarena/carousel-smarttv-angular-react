e7f7eb9bf8e7184d0b0fb3002ba5f3d3eece80bb
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-06-25 02:33:57 -0300

    update: webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,30 +1,25 @@
 var path = require('path');
 var webpack = require('webpack');
-var autoprefixer = require('autoprefixer');
 
 // App files location
 var PATHS = {
   app: path.resolve(__dirname, '../source/assets/js'),
-  styles: path.resolve(__dirname, '../source/assets/scss'),
-  html: path.resolve(__dirname, '../source/assets/html'),
   build: path.resolve(__dirname, '../public')
 };
 
 var plugins = [
-  // Avoid publishing files when compilation fails
   new webpack.NoErrorsPlugin(),
   new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('development'),
+    'process.env.NODE_ENV': JSON.stringify('production'),
     __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
   }),
-  new webpack.optimize.OccurenceOrderPlugin()
-];
-
-var sassLoaders = [
-  'style-loader',
-  'css-loader?sourceMap',
-  'postcss-loader',
-  'sass-loader?outputStyle=expanded'
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  })
 ];
 
 module.exports = {
@@ -43,7 +38,7 @@ module.exports = {
   },
   resolve: {
     // We can now require('file') instead of require('file.jsx')
-    extensions: ['', '.js', '.jsx', '.scss']
+    extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [
@@ -55,41 +50,9 @@ module.exports = {
           presets: ['react', 'es2015', 'stage-0']
         },
         include: PATHS.app
-      },
-      {
-        test: /\.scss$/,
-        loader: sassLoaders.join('!'),
-        include: PATHS.style
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader!postcss-loader'
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader"
-      },
-      // Inline base64 URLs for <=8k images, direct URLs for the rest
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-        loader: 'url-loader?limit=8192'
-      },
-      {
-        test: /\.(eot|ttf|wav|mp3)$/,
-        loader: 'file-loader'
       }
     ]
   },
   plugins: plugins,
-  postcss: function () {
-    return [autoprefixer({
-      browsers: ['last 2 versions']
-    })];
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, PATHS.build),
-    port: 8000,
-    historyApiFallback: true
-  },
-  devtool: 'eval'
+  devtool: 'source-map'
 };
--- source/tasks/gruntjs/webpack.js
+++ source/tasks/gruntjs/webpack.js
@@ -9,17 +9,6 @@ module.exports = function(grunt, options){
 
   return {
     options: webpackConfig,
-    build: {
-      plugins: webpackConfig.plugins.concat(
-        new webpack.DefinePlugin({
-          "process.env": {
-            // This has effect on the react lib size
-            "NODE_ENV": JSON.stringify("production")
-          }
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin()
-      )
-    }
+    build: {}
   };
 };
